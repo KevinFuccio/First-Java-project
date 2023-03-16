1dd9685bc767bbe547a1bc2a65f8bf2b8d25c79e
kevin fuccio <kevin.fuccio00@gmai.com>
2023-03-16 16:39:43 +0100

    d

--- giorno_4/src/pack/GestioneDipendenti.java
+++ giorno_4/src/pack/GestioneDipendenti.java
@@ -8,22 +8,22 @@ public class GestioneDipendenti {
 		Dipendente d3 = new Dipendente(Dipartimento.AMMINISTRAZIONE,Livello.IMPIEGATO);
 		Dipendente d4 = new Dipendente(Dipartimento.VENDITE,Livello.DIRIGENTE);
 		
-//		d1.promuovi();
-//		d3.promuovi();
-//		
-//		d1.infoDipendente();
-//		d2.infoDipendente();
-//		d3.infoDipendente();
-//		d4.infoDipendente();
-//		
-//		System.out.println(
-//		"somma della paga dei dipendenti: " + 
-//		(Dipendente.calcolaPaga(d1, 5)+ 
-//		 Dipendente.calcolaPaga(d2, 5)+ 
-//		 Dipendente.calcolaPaga(d3, 5)+ 
-//		 Dipendente.calcolaPaga(d4, 5))
-//		);
-		Dipendente.calcolaPaga(d4);
+		d1.promuovi();
+		d3.promuovi();
+		
+		d1.infoDipendente();
+		d2.infoDipendente();
+		d3.infoDipendente();
+		d4.infoDipendente();
+		
+		System.out.println(
+		"somma della paga dei dipendenti: " + 
+		(Dipendente.calcolaPaga(d1, 5)+ 
+		 Dipendente.calcolaPaga(d2, 5)+ 
+		 Dipendente.calcolaPaga(d3, 5)+ 
+		 Dipendente.calcolaPaga(d4, 5))
+		);
+		
 	}
 
 }
