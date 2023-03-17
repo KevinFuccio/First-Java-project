0292171a89573b54f15a54ffc598cbb1850386e8
kevin fuccio <kevin.fuccio00@gmai.com>
2023-03-17 17:01:21 +0100

    k

--- progetto_settimana_1/src/PACK_PROGETTO/main.java
+++ progetto_settimana_1/src/PACK_PROGETTO/main.java
@@ -5,30 +5,87 @@ import java.util.Scanner;
 public class main {
 	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("inserisci titolo");
-		String title = sc.nextLine();
-		System.out.println("durata");
-		int durata = sc.nextInt();
-		System.out.println("luminosita");
-		int luminosita = sc.nextInt();
+
+		multimediale[] m = new multimediale[2];
 		
-		
-		immagine i = new immagine(title,luminosita);
-		
-		
-		i.show();
+		codice(m);
 		
 		
 		
 		
 		
 		
-//		multimediale[] m = new multimediale[5];
+		
 //		m[0]= i;
 //		if(m[0] instanceof immagine) {
 //			immagine y = ((immagine)m[0]);
 //			y.prova();
 //		}
+		
+	}
+	public static void codice(multimediale[] m) {
+		for(int i =0; i < m.length; i++) {
+			System.out.println("seleziona che tipologia vuoi visualizzare 1: immagine | 2: audio | 3: video");
+			int tipologia = sc.nextInt();
+			System.out.println("inserisci titolo");
+			String title = sc.next();
+			switch(tipologia) {
+			case 1:
+				System.out.println("luminosita");
+				int luminosita = sc.nextInt();
+				immagine img = new immagine(title,luminosita);
+				m[i] = img;
+				break;
+			case 2:
+				System.out.println("inserisci durata");
+				int durata = sc.nextInt();
+				audio audio = new audio(title,durata);
+				m[i] = audio;
+				break;
+			case 3:
+				System.out.println("inserisci durata");
+				durata = sc.nextInt();
+				video video = new video(title,durata);
+				m[i] = video;
+				break;
+			default:
+				System.out.println("tipo non valido, riprova.");
+				i--;
+			}
+						
+		}
+		int elemento = -1;
+		
+		while(elemento != 0) {
+			System.out.println("Seleziona l'elemento che vuoi visualizzare. Premi 0 per uscire ");
+			elemento = sc.nextInt();
+			
+			if(elemento >= 1 && elemento <= m.length) {
+				multimediale oggetto = m[elemento -1];
+				
+				if(oggetto instanceof immagine) {
+					immagine img = (immagine)oggetto;
+					img.show();
+					
+				}else if(oggetto instanceof audio) {
+					audio aux = (audio)oggetto;
+					aux.play();
+					
+				}else if(oggetto instanceof video) {
+					video vid = (video)oggetto;
+					vid.play();
+					
+				}else{
+					System.out.println("l'elemento che cerchi non e corretto");
+				}
+			
+			}else {
+				System.out.println("perche sei uscito? :(");
+				sc.close();
+			}
+			
+		}
+			
 		
 	}
 
